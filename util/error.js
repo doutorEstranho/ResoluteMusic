07621a8028d666047ee6f63ccadaea5de850724e
Spray <71204668+Spray7@users.noreply.github.com>
2021-01-01 04:49:49 -0300

    Update error.js

--- util/error.js
+++ util/error.js
@@ -1,9 +1,9 @@
 const { MessageEmbed } = require("discord.js")
 
 /**
- * Easy to send errors because im lazy to do the same things :p
- * @param {String} text - Message which is need to send
- * @param {TextChannel} channel - A Channel to send error
+ * 
+ * @param {String} text 
+ * @param {TextChannel} channel 
  */
 module.exports = async (text, channel) => {
     let embed = new MessageEmbed()

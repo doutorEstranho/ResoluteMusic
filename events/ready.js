300c584d94bf2df4a8c99a936df4d5495940e174
Spray <71204668+Spray7@users.noreply.github.com>
2021-01-01 04:50:52 -0300

    Status
    LISTENING, WATCHING, PLAYING, STREAMING

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log("Estou Online.")
   await client.user.setActivity(`prefixo: s.help | Meu dono é o Spray :)`, {
-    type: "LISTENING",//can be LISTENING, WATCHING, PLAYING, STREAMING
+    type: "LISTENING",//LISTENING, WATCHING, PLAYING, STREAMING
   });
 };

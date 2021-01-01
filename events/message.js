532629285c6e7540c2e85cfea8de810c58823ce0
Spray <71204668+Spray7@users.noreply.github.com>
2021-01-01 04:50:17 -0300

    Update message.js

--- events/message.js
+++ events/message.js
@@ -8,10 +8,10 @@ module.exports = async (client, message) => {
   if (message.content.indexOf(prefix) !== 0) return;
 
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  //Making the command lowerCase because our file name will be in lowerCase
+ 
   const command = args.shift().toLowerCase();
 
-  //Searching a command
+  
   const cmd = client.commands.get(command);
   //Searching a command aliases
   const aliases = client.commands.find(x => x.info.aliases.includes(command))

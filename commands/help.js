651f1b9f4e8ed5913f970c25536c4a0d67f69be9
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-07 19:43:30 -0300

    💻 Arrumando

--- commands/help.js
+++ commands/help.js
@@ -1,44 +1,23 @@
-const { MessageEmbed } = require('discord.js')
+const Discord = require('discord.js');
+exports.run = async (bot, message, args) => {
+  let embed = new Discord.MessageEmbed()
+    .setColor('#e1ff00')
+    .setDescription(`> ***Olá ${message.author}, aqui estão meus comandos de música!***
+      \n » **Comandos:**\n
+      > **<:spr4yxyz:837798446584168468> | s.**
+      > **<:spr4yxyz:837798446584168468> | s.depositar**
+      > **<:spr4yxyz:837798446584168468> | s.leaderboard**
+      > **<:spr4yxyz:837798446584168468> | s.money**
+      > **<:spr4yxyz:837798446584168468> | s.pay**
+      > **<:spr4yxyz:837798446584168468> | s.roubar**
+      > **<:spr4yxyz:837798446584168468> | s.saque**
+      > **<:spr4yxyz:837798446584168468> | s.setmoney (Admin)**
+      > **<:spr4yxyz:837798446584168468> | s.trabalhar**
+      \n » **Links:**
+      > http://spr4y.xyz/resolute
+    `)
+    .setImage("http://pa1.narvii.com/5763/85377e06886cbaa577b87952dd985919f3ad0e38_00.gif")
+    .setThumbnail("https://media0.giphy.com/media/l2SpN0gAfO6yfw4A8/source.gif")  
 
-module.exports = {
-    info: {
-        name: "help",
-        description: "Informações de comandos",
-        usage: "",
-        aliases: "",
-    },
-
-    run: async function(client, message, args){
-        var allcmds = "";
-
-        client.commands.forEach(cmd => {
-            let cmdinfo = cmd.info
-            allcmds+="``"+client.config.prefix+cmdinfo.name+" "+cmdinfo.usage+"`` ~ "+cmdinfo.description+"\n"
-        })
-
-        let embed = new MessageEmbed()
-        .setAuthor("Comandos "+client.user.username, "https://cdn.discordapp.com/attachments/754170388115619872/784270025577463849/7b7a49c20326e3cbfdccb534ca0366f1.gif")
-        .setColor("BLUE")
-        .setDescription(allcmds)
-        .setFooter(`Para obter informações Complexas de cada comando ultilize | s.help <nome do comando>`)
-
-        if(!args[0])return message.channel.send(embed)
-        else {
-            let cmd = args[0]
- 
-            let command = client.commands.get(cmd)
-            if(!command)command = client.commands.find(x => x.info.aliases.includes(cmd))
-            if(!command)return message.channel.send("Comando invalido")
-            let commandinfo = new MessageEmbed()
-            .setTitle("Comando: "+command.info.name+" info")
-            .setColor("YELLOW")
-            .setDescription(`
-Nome: ${command.info.name}
-Descrição: ${command.info.description}
-Use: \`\`${client.config.prefix}${command.info.name} ${command.info.usage}\`\`
-Apelido: ${command.info.aliases.join(", ")}
-`)
-            message.channel.send(commandinfo)
-        }
-    }
+    message.channel.send(embed)
 }

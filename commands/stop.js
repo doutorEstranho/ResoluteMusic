9893bd8aaa9023822492cbdbdc6afde38ec40574
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-11 15:11:29 -0300

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -4,16 +4,16 @@ const sendError = require("../util/error");
 module.exports = {
   info: {
     name: "stop",
-    description: "Para parar a musica.",
+    description: "Parar a musica.",
     usage: "",
     aliases: [],
   },
 
   run: async function (client, message, args) {
     const channel = message.member.voice.channel
-    if (!channel)return sendError("Sinto muito, mas você precisa estar em um canal de voz para tocar música!", message.channel);
+    if (!channel)return sendError("<:info:835206734225473546> | Sinto muito, mas você precisa estar em um canal de voz para tocar música!", message.channel);
     const serverQueue = message.client.queue.get(message.guild.id);
-    if (!serverQueue)return sendError("Não há nada que eu possa pular para você.", message.channel);
+    if (!serverQueue)return sendError("<a:__:835643988022263879> | Não há nada que eu possa pular para você.", message.channel);
    if(!serverQueue.connection)return
 if(!serverQueue.connection.dispatcher)return
      try{
@@ -21,7 +21,7 @@ if(!serverQueue.connection.dispatcher)return
       } catch (error) {
         message.guild.me.voice.channel.leave();
         message.client.queue.delete(message.guild.id);
-        return sendError(`:notes: O jogador parou e a fila foi liberada. ${error}`, message.channel);
+        return sendError(`<a:__:835643988022263879> | O jogador parou e a fila foi liberada. ${error}`, message.channel);
       }
     message.client.queue.delete(message.guild.id);
     serverQueue.songs = [];

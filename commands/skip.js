902fff4b04f64442ce1a7e1ab1900deed7fa7558
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-11 15:12:41 -0300

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -11,18 +11,18 @@ module.exports = {
 
   run: async function (client, message, args) {
     const channel = message.member.voice.channel
-    if (!channel)return sendError("Sinto muito, mas você precisa estar em um canal de voz para tocar música!", message.channel);
+    if (!channel)return sendError("<:info:835206734225473546> | Sinto muito, mas você precisa estar em um canal de voz para tocar música!", message.channel);
     const serverQueue = message.client.queue.get(message.guild.id);
-    if (!serverQueue)return sendError("Não há nada que eu possa pular para você.", message.channel);
+    if (!serverQueue)return sendError("<:info:835206734225473546> | Não há nada que eu possa pular para você.", message.channel);
         if(!serverQueue.connection)return
 if(!serverQueue.connection.dispatcher)return
      if (serverQueue && !serverQueue.playing) {
       serverQueue.playing = true;
       serverQueue.connection.dispatcher.resume();
       let xd = new MessageEmbed()
-      .setDescription("Musica despausada!")
+      .setDescription("<:info:835206734225473546> | Musica despausada!")
       .setColor("YELLOW")
-      .setTitle("Musica foi despausada")
+      .setTitle("Musica foi despausada.")
        
    return message.channel.send(xd).catch(err => console.log(err));
       
@@ -34,7 +34,7 @@ if(!serverQueue.connection.dispatcher)return
       } catch (error) {
         serverQueue.voiceChannel.leave()
         message.client.queue.delete(message.guild.id);
-        return sendError(`:notes: O jogador parou e a fila foi liberada. ${error}`, message.channel);
+        return sendError(`<:info:835206734225473546> |  O jogador parou e a fila foi liberada. ${error}`, message.channel);
       }
     message.react("✅")
   },

3ca00454f0f286106f421ef8d452e7409f4d978b
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-11 15:06:05 -0300

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,20 +1,13 @@
 module.exports = async (client, message) => {
   if (message.author.bot) return;
-
-  
   const prefixMention = new RegExp(`^<@!?${client.user.id}> `);
   const prefix = message.content.match(prefixMention) ? message.content.match(prefixMention)[0] : client.config.prefix;
-
   if (message.content.indexOf(prefix) !== 0) return;
-
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
- 
   const command = args.shift().toLowerCase();
-
   const cmd = client.commands.get(command);
-
   const aliases = client.commands.find(x => x.info.aliases.includes(command))
-
+  
 process.on("unhandledRejection", (reason, promise) => {
     try {
         console.error("Unhandled Rejection at: ", promise, "reason: ", reason.stack || reason);

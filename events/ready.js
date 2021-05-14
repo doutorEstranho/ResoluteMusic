d49695f98750505088c3de3852619690cfcb07fe
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-14 12:36:00 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`Online em ${client.guilds.cache.size}`)
-  await client.user.setActivity(`prefixo: s.help`, {
+  await client.user.setActivity(`resolutebot.xyz`, {
     type: "LISTENING",//LISTENING, WATCHING, PLAYING, STREAMING
   });
 };

0a7882b1a912837401e0ac9af00656fbfa4c3438
sprayx <71204668+sprayx@users.noreply.github.com>
2021-05-14 12:36:10 -0300

    Update error.js

--- util/error.js
+++ util/error.js
@@ -9,6 +9,6 @@ module.exports = async (text, channel) => {
     let embed = new MessageEmbed()
     .setColor("RED")
     .setDescription(text)
-    .setFooter("Resolute - erro")
+    .setFooter("Resolute")
     await channel.send(embed)
 }
